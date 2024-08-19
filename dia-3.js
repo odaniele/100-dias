df63f971c04b2e153ec6c009a71461b6d2ab8dac
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-08-19 20:55:44 -0300

    dia-3

---
+++ dia-3.js
@@ -0,0 +1,6 @@
+const entrada = require ('prompt-sync');
+
+let num1 = parseInt(entrada("Informe um número: "));
+let num2 = parseInt(entrada("Informe outro número: "));
+soma = (num1 + num2);
+console.log("A soma dos número é igual a: ", soma);

40b5055f0d21c840e034375133c43255fdf5e62c
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-11-19 22:58:51 -0300

    dia-29 variaveis

---
+++ 21-30/dia-29.js
@@ -0,0 +1,5 @@
+const entrada = require ('prompt-sync')({sigint:true});
+
+let horasTrabalhadas = parseFloat(entrada('Quantas horas você trabalhou neste mês?'));
+let valorHora = parseFloat(entrada('Qual o valor da sua hora de trabalho?'));
+

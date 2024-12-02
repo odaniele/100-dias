0fdf958d3f211ec37cef3b722dee6eff222bd2f8
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-12-01 21:47:08 -0300

    dia-40

--- 31-40/dia-40.js
+++ 31-40/dia-40.js
@@ -2,13 +2,30 @@ const entrada = require ('prompt-sync')({sigint: true});
 
 let num1 = parseFloat(entrada('Informe o primeiro número da operação: '));
 let num2 = parseFloat(entrada('Informe o segundo número da operação: '));
-let operacao = String(entrada('Qual operação deseja realizar? '));
+let operacao = String(entrada('Qual operação deseja realizar (+, -, *, /)? '));
 
-let resultado = 0;
-let sinal = '';
-let paridade = '';
-let natureza = '';
+let resultado;
+let erro = false;
 
+if (operacao === '+') {
+    resultado = num1 + num2;
+} else if (operacao === '-') {
+    resultado = num1 - num2;
+} else if (operacao === '*') {
+    resultado = num1 * num2;
+} else if (operacao === '/') {
+    if (num2 !== 0) {
+    resultado = num1 / num2;
+    } else {
+    console.log('Erro: Divisão por zero');
+    erro = true;
+    }    
+}
 
-console.log(`O resultado da operação é: ${resultado}\n
-    Este número é ${paridade}\n${sinal}\ne ${natureza}`);
+if(!erro) {
+let sinal = (resultado > 0) ? 'positivo' : 'negativo';
+let paridade = (resultado % 2 === 0) ? 'par' : 'ímpar';
+let natureza = (resultado % 1 === 0) ? 'inteiro' : 'decimal';
+
+console.log(`O resultado da operação é: ${resultado}\nEste número é ${paridade}, ${sinal} e ${natureza}.`);
+}

182262706bbb8f7f65fc666239ee5a459c33f3d1
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2025-01-10 21:42:57 -0300

    dia-80

---
+++ 71-80/dia-80.js
@@ -0,0 +1,37 @@
+const entrada = require ('prompt-sync')({ sigint: true });
+
+function caixaRegistradora() {
+    while (true) {
+        let totalCompra = 0;
+        let produtoCount = 1;
+
+        console.log('Loja Tabajara');
+
+        while (true) {
+            let precoProduto = parseFloat(entrada(`Produto ${produtoCount}: R$`));
+
+            if (precoProduto === 0) {
+                break;
+            }
+
+            totalCompra += precoProduto;
+            produtoCount++;
+        }
+
+    console.log(`Total: R$ ${totalCompra.toFixed(2)}`);
+
+    let dinheiroCliente = parseFloat(entrada('Dinheiro: R$'));
+
+    let troco = dinheiroCliente - totalCompra;
+    console.log(`Troco: R$ ${troco.toFixed(2)}`);
+
+    let continuar = entrada('Deseja registrar outra compra? (sim/não) ').toLowerCase();
+
+        if (continuar !== 'sim') {
+            break;
+        }
+
+    }
+}
+
+caixaRegistradora();
